a0ecc803b7cfe0d115a08c235bb5dd264fd5667a
Dario George <edu.dariogeorge21@gmail.com>
2025-06-13 12:29:10 +0530

    hooks + input hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [Location,setLocation] = useState('');
+  const [inputData, setinputData] = useState('');
+  const [WeatherStats, setWeatherStats] = useState<null | {
+    location: String;
+    current: Object;
+  }>(null);
+  const [Celsius,setCelsius] = useState('');
+  const [Fahrenheit,setFarenheit] = useState('');
 
+  
   return (
     <>
       <header className="heading">Weather API APP</header>
       <br /><br />
       <div className="formContainer">
-        <input type="text" id='inputData' placeholder=' Enter Location: '/>
+        <input type="number" className='inputField' placeholder='Enter Location' value={inputData} onChange={(e) => setinputData(e.target.value)} />
         <button>Check</button>
+        
+      </div>
+      <hr className='hr'/>
+      <br /><br />
+      <div className="stats">
+        <p className="location">Location: </p>
+        <p className="temperatureCelsius">Temperature in Celsius: </p>
+        <p className="temperatureFahrenheit">Temperature in Fahrenheit: </p>
+        <p className="humidity">Humidity: </p>
 
       </div>
     </>
