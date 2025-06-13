b60b8b77bc91dc1339946402ba020e13bbe109ba
Dario George <edu.dariogeorge21@gmail.com>
2025-06-13 13:42:10 +0530

    api url http->https

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     try{
       const location = inputData;
       const query = location.trim().replace(/\s+/g, '+');
-      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_WEATHER_API_KEY}&q=${query}`);
+      const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_WEATHER_API_KEY}&q=${query}`);
       if (!response.ok){
         throw new Error("Failed to fetch from the API! Try some other location!");
       }
