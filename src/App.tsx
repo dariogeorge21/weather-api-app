71c925d3382e3b279d226e3d7a0a43055cdfd3be
Dario George <edu.dariogeorge21@gmail.com>
2025-06-13 12:04:21 +0530

    base UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ function App() {
 
   return (
     <>
-      
+      <header className="heading">Weather API APP</header>
+      <br /><br />
+      <div className="formContainer">
+        <input type="text" id='inputData' placeholder=' Enter Location: '/>
+        <button>Check</button>
+
+      </div>
     </>
   )
 }
